3c118d459a4274c50a72a4afdf81a7e997e5f4d5
Alex Kasa <vailveix@gmail.com>
2018-10-01 11:17:37 -0400

    Quiz #8 My Answers

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.text.NumberFormat;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -13,11 +15,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void submitOrder(){
-        display(100);
+        display(2);
+        displayPrice(2*5);
     }
 
     private void display(int number){
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
+    }
+
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 }
