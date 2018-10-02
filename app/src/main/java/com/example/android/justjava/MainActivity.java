79a514c1c9c91e255574c316871052beecf2ec31
Alex Kasa <vailveix@gmail.com>
2018-10-02 16:11:07 -0400

    Lesson 6 Quiz #3
    Checkbox use

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.android.justjava;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 import java.text.NumberFormat;
@@ -12,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     private int quantity = 0;
     private int price = 5;
     private int userPrice = 0;
+    private boolean toppingAdd = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,17 +33,22 @@ public class MainActivity extends AppCompatActivity {
         display(quantity);
     }
 
-    private String createOrderSummary(int number){
+    private String createOrderSummary(int number, boolean toppingAdd){
         String orderSummary ="Name: Skips Snow";
+        orderSummary += "\nAdd Whipped Cream: " + toppingAdd;
         orderSummary += "\nQuantity: " + quantity;
         orderSummary += "\nThank You!";
         return orderSummary;
     }
 
     public void submitOrder(View view){
+        CheckBox toppingBox = (CheckBox) findViewById(R.id.toppings);
+
+        toppingAdd = toppingBox.isChecked();
         userPrice = price*quantity;
+
         displayPrice(price*quantity);
-        displayMessage(createOrderSummary(price*quantity));
+        displayMessage(createOrderSummary(price*quantity, toppingAdd));
     }
 
     private void display(int number){
